f6fc9d64a3db12e3a187df4360635a52b2b7be6b
forever24-kobe <77265138+forever24-kobe@users.noreply.github.com>
2023-03-07 10:29:27 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("hot-fix test!");
-        System.out.println("master test!");
-        System.out.println("master test!");
-        System.out.println("master test!");
-        System.out.println("master test!");
+        System.out.println("Hello shuaibiteng!");
     }
 }
